83050748735f85db89ef71bb7e2da0fb222fa9bf
panariellop <pierodpanariello@gmail.com>
2020-08-31 13:12:51 -0400

    responsive to page visited

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,47 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import './Navbar.css';
-const logo = '../static/logo.ico';
+
 class Navbar extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      active_pane: "home"
+    }
+  }
+
+
+
   render(){
+
+    const active_style = {
+      color: 'blue'
+    }
+
 		return(
 			<div className = "navbar-container">
 				<h3 className = "navbar-logo">Piero Panariello</h3>
 					<ul className = "navbar-navlist">
 						<li className = "navbar-navlink">
-							<Link className = "navbar-routerlink" to = '/'>Home</Link>
+							<Link style = {this.state.active_pane === 'home'? active_style: null} onClick = {() => {
+                  this.setState({
+                    active_pane: 'home'
+                  })
+                }} className = "navbar-routerlink" to = '/'>Home</Link>
 						</li>
 						<li className = "navbar-navlink">
-							<Link className = "navbar-routerlink" to = '/portfolio'>Portfolio</Link>
+							<Link style = {this.state.active_pane === 'portfolio'? active_style: null} onClick = {() => {
+                  this.setState({
+                    active_pane: 'portfolio'
+                  })
+                }} className = "navbar-routerlink" to = '/portfolio'>Portfolio</Link>
 						</li>
 						<li className = "navbar-navlink">
-							<Link className = "navbar-routerlink" to = '/contact'>Contact</Link>
+							<Link style = {this.state.active_pane === 'contact'? active_style: null} onClick = {() => {
+                  this.setState({
+                    active_pane: 'contact'
+                  })
+                }} className = "navbar-routerlink" to = '/contact'>Contact</Link>
 						</li>
 					</ul>
 			</div>
@@ -23,4 +49,4 @@ class Navbar extends React.Component{
 	}
 };
 
-export default Navbar 
+export default Navbar
