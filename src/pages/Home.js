18c0c0def04734f018a754c730f26637ce691b60
panariellop <pierodpanariello@gmail.com>
2020-12-17 19:07:26 -0500

    spelling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -32,11 +32,11 @@ const Home = () =>{
       </div>
       <div className = "home-bio">
       <h3>Bio</h3>
-      	<p>I am presently attending the College of William and Mary persuing a B.S. in Computer Science and Physics.</p>
+      	<p>I am presently attending the College of William and Mary pursuing a B.S. in Computer Science and Physics.</p>
     	<br/>
 	<p>Growing up, my younger self always had an Idea for a new project related to engineering and computer science. Whether it was programming an Arduino Uno, building a rocket nozzle, designing a wind tunnel, or creating a game in Unity, I loved the challenge and freedom programming and engineering gave me.</p>
     	<br/>
-    	<p>The combination of studying Computer Science and Physics continues to broaden the depth of my techical and analytical skills. I look forward to investing my talents and knowledge to effect positive change in the world and the lives of others.</p>
+    	<p>The combination of studying Computer Science and Physics continues to broaden the depth of my techical and analytical skills. I look forward to investing my talents and knowledge to effect positive change while I launch my career.</p>
       </div>
       
     </div>
