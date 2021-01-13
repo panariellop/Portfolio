288d00db79a5f6504bbd116af34a91a04a8fee95
panariellop <pierodpanariello@gmail.com>
2021-01-13 11:50:50 -0500

    finished menu bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import './Navbar.css';
+import hamburger from "../static/hamburger.png"
 
 class Navbar extends React.Component{
   constructor(props){
@@ -31,30 +32,7 @@ class Navbar extends React.Component{
 		return(
 			<div className = "navbar-container">
 				<h3 className = "navbar-logo">Piero Panariello</h3>
-					<ul className = {"navbar-navlist-" + (this.state.showMenu ? "false" : "true")}>
-						<li className = "navbar-navlink">
-							<Link style = {this.state.active_pane === 'home'? active_style: null} onClick = {() => {
-                  this.setState({
-                    active_pane: 'home'
-                  })
-                }} className = "navbar-routerlink" to = '/'>Home</Link>
-						</li>
-						<li className = "navbar-navlink">
-							<Link style = {this.state.active_pane === 'portfolio'? active_style: null} onClick = {() => {
-                  this.setState({
-                    active_pane: 'portfolio'
-                  })
-                }} className = "navbar-routerlink" to = '/portfolio'>Portfolio</Link>
-						</li>
-						<li className = "navbar-navlink">
-							<Link style = {this.state.active_pane === 'contact'? active_style: null} onClick = {() => {
-                  this.setState({
-                    active_pane: 'contact'
-                  })
-                }} className = "navbar-routerlink" to = '/contact'>Contact</Link>
-						</li>
-					</ul>
-          <button onClick = {this.showMenu}>+</button>
+          <button onClick = {this.showMenu}><img src = {hamburger} width = "30px" height = "30px"/></button>
           <div className = {"navbar-navmenu-" + this.state.showMenu}>
             <ul className = "navbar-navmenu-container">
                 <li className = "navbar-menulink">
