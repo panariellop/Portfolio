12caa8c3dac3fa1f1b06e6d7dd3a9cfe35f7efc3
panariellop <pierodpanariello@gmail.com>
2021-06-07 19:21:17 -0400

    u

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,20 +12,51 @@ class Navbar extends React.Component{
     this.state = {
       active_pane: "home", 
     }
+      this.handleChange = this.handleChange.bind(this)
   }
 
+    
+    handleChange(e){
+        this.setState({
+            active_pane: e.target.name
+        })
+
+    }
 
 
   render(){
+        const active_link = {
+            textDecoration: "underline"
+        }
+
       return(
           <Fragment>
           <Router>
             <div className = "navbar">
                 <div className = "menu">
                     <ul>
-                        <li><Link to = "/" className = "link">home</Link></li>
-                        <li><Link to = "/portfolio" className = "link">portfolio</Link></li>
-                        <li><Link to = "/contact" className = "link">contact</Link></li>
+                        <li><Link 
+                                style = { this.state.active_pane === "home" ? active_link : null}
+                                to = "/" 
+                                className = "link"
+                                onClick = {this.handleChange}
+                                name = "home"
+                        >home</Link></li>
+                        <li><Link 
+                                 style = { this.state.active_pane === "portfolio" ? active_link : null}
+
+                                 to = "/portfolio" 
+                                 className = "link"
+                                 name = "portfolio"
+                                 onClick = {this.handleChange}
+                                 >portfolio</Link></li>
+                        <li><Link 
+                                style = { this.state.active_pane === "contact" ? active_link : null}
+                                 to = "/contact" 
+                                 className = "link"
+                                 name = "contact"
+                                 onClick = {this.handleChange}
+                                 >contact</Link></li>
                     </ul>
                 </div>
                 <div className = "name">
